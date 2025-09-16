0796ab070df2b89e1fda4584a0a72793dfe153fd
customercarealife <customercare.alife@gmail.com>
2025-09-16 14:53:28 +0630

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ from email.message import EmailMessage
 from flask import Flask, render_template, request, redirect, flash, send_file, after_this_request
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
+from dotenv import load_dotenv
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 
